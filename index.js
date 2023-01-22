fcb981ba806c711829c8dea4e7dded1d32b7b120
georgymukha <georgymukha@yandex.ru>
2023-01-22 20:36:14 +0600

    add express

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/instagram/:username/followers", (req, res) => {
+  const { username } = req.params;
+
+  fetch(`https://www.instagram.com/${username}/followers/`)
+    .then((response) => response.json())
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
+
 async function checkNonReciprocals(username) {
   // Fetch the user's followers
   const followersResponse = await fetch(
-    `https://www.instagram.com/${username}/followers/`
+    `http://localhost:3000/instagram/${username}/followers`
   );
   const followersData = await followersResponse.json();
   const followers = followersData.users.map((user) => user.username);
